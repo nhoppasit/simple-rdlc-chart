a70369b04108765a31bceeacba1f62140d6d05a4
Nhoppasit <dev.theprogram@gmail.com>
2018-09-08 16:16:12 +0700

    simple chart with decimal display and series

---
+++ RDLC.SimpleChart V1/RDLC.SimpleChart V1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RDLC.SimpleChart_V1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Table from database
+            List<Model.SimpleChart> list = new List<Model.SimpleChart>
+            {
+                new Model.SimpleChart() {ID=0, SERIES="AAA", X1=1.5m,Y1=0.5m,X2=1,Y2=0.5m},
+                new Model.SimpleChart() {ID=1, SERIES="AAA", X1=2.5m,Y1=0.3m,X2=2,Y2=0.2m},
+                new Model.SimpleChart() {ID=2, SERIES="AAA", X1=3.5m,Y1=0.6m,X2=3,Y2=0.3m},
+                new Model.SimpleChart() {ID=3, SERIES="AAA", X1=4.5m,Y1=0.5m,X2=4,Y2=0.7m},
+
+                new Model.SimpleChart() {ID=4, SERIES="BBB", X1=3.5m,Y1=0.5m,X2=1,Y2=0.5m},
+                new Model.SimpleChart() {ID=5, SERIES="BBB", X1=4.5m,Y1=0.3m,X2=2,Y2=0.2m},
+                new Model.SimpleChart() {ID=6, SERIES="BBB", X1=5.5m,Y1=0.6m,X2=3,Y2=0.3m},
+                new Model.SimpleChart() {ID=7, SERIES="BBB", X1=6.5m,Y1=0.5m,X2=4,Y2=0.7m},
+            };
+            ReportDataSource rd1 = new ReportDataSource("SimpleData", list);
+
+            // assing and refresh report
+            reportViewer1.LocalReport.DataSources.Clear();
+            reportViewer1.LocalReport.DataSources.Add(rd1);
+            reportViewer1.LocalReport.Refresh();
+            reportViewer1.SetDisplayMode(DisplayMode.PrintLayout);  //ปรับเป็นหน้า PrintLayout
+            reportViewer1.ZoomMode = ZoomMode.Percent;
+            reportViewer1.ZoomPercent = 100;
+            reportViewer1.RefreshReport();
+
+            //this.reportViewer1.RefreshReport();
+        }
+    }
+}
---
+++ RDLC.SimpleChart V1/RDLC.SimpleChart V1/Model/SimpleChart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDLC.SimpleChart_V1.Model
+{
+    public class SimpleChart
+    {
+        public int ID { get; set; }
+        public string SERIES { get; set; }
+        public decimal X1 { get; set; }
+        public decimal X2 { get; set; }
+        public decimal Y1 { get; set; }
+        public decimal Y2 { get; set; }
+        public string COMMENT { get; set; }
+    }
+}
